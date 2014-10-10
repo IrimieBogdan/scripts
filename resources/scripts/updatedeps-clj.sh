aa5dff8729df6565c2e28bafc7b0203d04d7ac9b
Wayne <wayne@puppetlabs.com>
2014-10-10 13:41:19 -0700

    (MAINT) Fix bug in updatedeps-clj.sh script.
    We should only ever update dependencies if we are on the master branch. If on
    some other branch, then when we push to master at a later point in the job all
    hell will break loose.
    
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- resources/scripts/updatedeps-clj.sh
+++ resources/scripts/updatedeps-clj.sh
@@ -8,9 +8,15 @@ set +x
 
 if [ ! -z "$DEP_VERSION" ] && [ ! -z "$DEP_NAME" ] ;then
 
+    if [ ! "$GIT_VER" = "master" ] ;then
+        echo "GIT_VER must be 'master' when changing dependency versions."
+        exit 1
+    fi
+
     #---------------------------------------------------------------------------
     # Run dependency-updating script
     ./utils/update-dep.sh {name}/project.clj $DEP_VERSION $DEP_NAME puppetlabs
+
 else
     echo "WARNING: Continuing without updating any dependency version."
 fi
