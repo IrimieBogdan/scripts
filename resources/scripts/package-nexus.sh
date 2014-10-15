fb0f0fdea600c551a563632fdb7868bc3cedc7cb
Wayne <wayne@puppetlabs.com>
2014-10-14 17:02:09 -0700

    Bug fixes on puppet-server pipeline.
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- resources/scripts/package-nexus.sh
+++ resources/scripts/package-nexus.sh
@@ -4,21 +4,25 @@ export NEXUS_JENKINS_USERNAME=jenkins
 export NEXUS_JENKINS_PASSWORD=Qu@lity!
 
 set -e
-
-LEIN=$(mktemp lein.XXXXXX)
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -O ${LEIN}
-chmod +x ${LEIN}
-
-./${LEIN:-lein} version
-./${LEIN:-lein} deploy
-
 set -x
-cat > packaging.props <<DOWNSTREAM_BUILD_PARAMETERS
-GIT_REF=$(git show-ref HEAD)
-LEIN_PROJECT_VERSION=$(lein with-profile ci pprint :version | tail -n 1 | cut -d\" -f2)
-DOWNSTREAM_BUILD_PARAMETERS
+
+echo
+echo "Show version information:"
+lein version
+
+echo
+echo "Deploying to maven repository:"
+lein with-profile ci pprint :repositories
+lein with-profile ci pprint :deploy-repositories
+
+lein deploy
+
+echo
+echo "Create .props file to pass LEIN_PROJECT_VERSION to next job."
 set +x
+cat > "$WORKSPACE/.props" <<PROPS
+LEIN_PROJECT_VERSION=$(lein with-profile ci pprint :version | tail -n 1 | cut -d\" -f2)
+PROPS
+set -x
 
-set +e # failed commands after this do not indicate job failure
-
-rm ${LEIN}
+cat "$WORKSPACE/.props"
