fc4f89ecf0e1d152f1253fd53115096386aef61c
Wayne <wayne@puppetlabs.com>
2014-10-21 13:39:32 -0700

    utils: Adds a shell script to call jenkins-jobs.
    Signed-off-by: Wayne <wayne@puppetlabs.com>

---
+++ utils/deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------------------------------
+# Command line parameters
+#
+
+COMMAND=${1:?}
+JENKINS_INSTANCE=${2:?}
+DEPLOYMENT_TYPE=${3:-staging}
+
+if [ ! "$DEPLOYMENT_TYPE" = "staging" ] && \
+	[ ! "$DEPLOYMENT_TYPE" = "production" ]
+then
+	echo "FAILURE: Invalid deployment type: \"${DEPLOYMENT_TYPE}\""
+	exit 1
+fi
+#-------------------------------------------------------------------------------
+# Setup
+#
+
+JJB_PATHS="resources:${JENKINS_INSTANCE}/resources:${JENKINS_INSTANCE}/${DEPLOYMENT_TYPE}"
+JJB_CONFIG="${JENKINS_INSTANCE}/builder.conf"
+
+echo "Running Jenkins Job Builder."
+echo
+echo "Jenkins instance: ${JENKINS_INSTANCE}"
+echo "Deployment type:  ${DEPLOYMENT_TYPE}"
+
+case $COMMAND in
+	update)
+	jenkins-jobs --conf ${JJB_CONFIG} update ${JJB_PATHS}
+	;;
+	test)
+	jenkins-jobs --conf ${JJB_CONFIG} test ${JJB_PATHS}
+	;;
+	*)
+	echo
+	echo "FAILURE: Unrecognized JJB command, \"$COMMAND\""
+	exit 1
+	;;
+esac
+
+exit 0
