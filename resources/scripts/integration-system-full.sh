44933c8bf2d92123c05cf2615b06b5e3d245c615
Wayne <wayne@puppetlabs.com>
2014-11-06 10:33:59 -0800

    puppet-server: Use submodule acceptance tests.
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- resources/scripts/integration-system-full.sh
+++ resources/scripts/integration-system-full.sh
@@ -3,33 +3,32 @@
 set -x
 
 #-------------------------------------------------------------------------------
-# Prepare external acceptance testing repository. Whoops, definitely not
-# necessary for FOSS since FOSS is a submodule of jvm-puppet. Leaving it here as
-# an example when getting started with puppetdb.
+# Prepare external acceptance testing repository.
 #
 
 mkdir -p ./tmp
-ACCEPTANCE_REPO="https://github.com/waynr/puppet"
-ACCEPTANCE_LOCALPATH="tmp/puppet"
-ACCEPTANCE_REF="${ACCEPTANCE_REF:-quickfix}"
+#ACCEPTANCE_REPO="https://github.com/waynr/puppet"
+#ACCEPTANCE_REF="${{ACCEPTANCE_REF:-quickfix}}"
 
-git clone "${ACCEPTANCE_REPO}" "${ACCEPTANCE_LOCALPATH}"
+ACCEPTANCE_LOCALPATH="ruby/puppet"
 
-if [ ! -z "${ACCEPTANCE_REF}" ] ;then
-	pushd "${ACCEPTANCE_LOCALPATH}"
-	git checkout "${ACCEPTANCE_REF}"
+#git clone "$ACCEPTANCE_REPO" "$ACCEPTANCE_LOCALPATH"
+
+if [ ! -z "$ACCEPTANCE_REF" ] ;then
+	pushd "$ACCEPTANCE_LOCALPATH"
+	git checkout "$ACCEPTANCE_REF"
 	popd
 fi
 
-export BEAKER_TESTSUITE="${ACCEPTANCE_LOCALPATH}/acceptance/tests"
+export BEAKER_TESTSUITE="$ACCEPTANCE_LOCALPATH/acceptance/tests"
 export BEAKER_POSTSUITE="acceptance/suites/post_suite"
-export BEAKER_LOADPATH="${ACCEPTANCE_LOCALPATH}/acceptance/lib"
+export BEAKER_LOADPATH="$ACCEPTANCE_LOCALPATH/acceptance/lib"
 
 #-------------------------------------------------------------------------------
 # Additional Beaker configuration.
 #
 
-export BEAKER_OPTS="--keyfile ${HOME}/.ssh/id_rsa-acceptance --xml --preserve-hosts ${PRESERVE_HOSTS}"
+export BEAKER_OPTS="--keyfile $HOME/.ssh/id_rsa-acceptance --xml --preserve-hosts $PRESERVE_HOSTS"
 export BEAKER_OPTIONSFILE="acceptance/config/beaker/options.rb"
 export BEAKER_TYPE=foss
 unset IS_PE
