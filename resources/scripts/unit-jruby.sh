7f102354e12b972872af1925c07d147746d76f14
Wayne <wayne@puppetlabs.com>
2015-01-23 10:37:17 -0800

    (puppet-server) Implement lein-ezbake ci pipeline.
    * implements the lein-ezbake pipeline and swaps it in for the old "lein
    deploy" + "uberbuild" jobs.
    * fixes the jruby unit tests but invoking rake within jruby rather than
    MRI context.
    * removes the puppet-server "future" branch pipeline.
    
    (MAINT) Cleanup jruby unit tests.
    
    * Enables the export of project-specific environment variables in jruby unit
      test. Actually this allows injection of arbitrary shell script into the job
      but it is intended primarily for the sake of exporting environment variables.
    * Unifies classifier and puppet-server jruby unit tests.
    
    (JJB-MAINT) Cleanup usages of 'kickoff_disabled' var.
    
    The ability to pass "True" or "true" to the disable job project setting is
    something I added to JJB because I did not know about the {obj:kickoff_disabled}
    idiom in JJB's yaml parsing system that allows python objects to used during
    variable interpolation (prior to template reification).
    
    The next update to my experimental branch of JJB will not include my pointless
    patch; before I can update that branch, this patch will need to be merged.
    
    (MAINT) Move pe-console-services jobs file.

--- resources/scripts/unit-jruby.sh
+++ resources/scripts/unit-jruby.sh
@@ -4,24 +4,48 @@ set -e
 set -x
 
 #-------------------------------------------------------------------------------
+# Set variable defaults
+
+export GEM_SOURCE=${{GEM_SOURCE:-{gem_source}}}
+export RVM_RUBY_VERSION=${{RVM_RUBY_VERSION:-{rvm_ruby_version}}}
+
+{additional_exports}
+
+#-------------------------------------------------------------------------------
 # Configure Ruby
 
 source /usr/local/rvm/scripts/rvm
-rvm use ruby-1.9.3-p484
+rvm use $RVM_RUBY_VERSION
 
+#-------------------------------------------------------------------------------
 # Set Java JDK version based on current matrix value of JDK_BIN_PATH which
 # should be a path to the bin directory of the desired JDK.
-#
 
-if [ ! -z "${JDK_BIN_PATH}" ] ;then
-	export PATH="${JDK_BIN_PATH:-}:${PATH}"
+if [ ! -z "$JDK_BIN_PATH" ] ;then
+    export PATH="$JDK_BIN_PATH:$PATH"
 fi
 
+#-------------------------------------------------------------------------------
+# Print interesting system information
+
+echo
+echo "Print Java version"
 java -version
 
-# Run JRuby tests.
-#
+echo
+echo "Install Leiningen 2.5.1"
+mkdir -p tmp/bin
+cd tmp/bin
+wget https://raw.githubusercontent.com/technomancy/leiningen/2.5.1/bin/lein
+chmod u+x lein
+cd ../../
+
+export PATH=$WORKSPACE/tmp/bin:$PATH
+
+echo
+echo "Print Leiningen version"
+./tmp/bin/lein version
+
 
 lein version
-rake spec
-
+lein run -m org.jruby.Main -e 'load "META-INF/jruby.home/bin/rake"' spec
