26776c6f9959b38954d32222b9477b9e9495a157
Wayne <wayne@puppetlabs.com>
2015-01-28 15:28:10 -0800

    (unit-js-ember-cli.sh) Bugfix in shell script.

--- resources/scripts/unit-js-ember-cli.sh
+++ resources/scripts/unit-js-ember-cli.sh
@@ -8,7 +8,7 @@ npm install
 npm install bower
 
 # alias the local npm bin directory
-alias npm-exec='PATH=$(npm bin):$PATH'
+export PATH=$(npm bin):$PATH
 
-npm-exec bower install
-npm-exec ember test --environment=ci
+bower install
+ember test --environment=ci
