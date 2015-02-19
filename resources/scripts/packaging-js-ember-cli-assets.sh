a24c8cadfdef607d49f33243d11c3799f75195bc
jonathannewman <jonathan.newman@puppetlabs.com>
2015-02-18 19:24:12 -0800

    (maint) fix UI compose and build job
    The UI compose and Build job has a script that references ember as an application.  This should be in the local npm bin directory.  Prior to this commit, the path to the local npm bin directory wasn't being included in the path, and thus the ember binary was unavailable.  This commit adds the local npm bin directory to the path.

--- resources/scripts/packaging-js-ember-cli-assets.sh
+++ resources/scripts/packaging-js-ember-cli-assets.sh
@@ -5,16 +5,26 @@ set -x
 
 echo
 echo "Remove old assets from the gitz"
-git rm -r $WORKSPACE/{assets-outdirs}
+cd $WORKSPACE
+git rm -r {assets-outdirs}
 
 echo
 echo "Entering build directory and building assets"
 cd $WORKSPACE/{assets-tempdir}
+
+npm install
+
+# list the npm package versions
+npm ls --depth 0
+
+# alias the local npm bin directory -- this is needed for the ember app
+export PATH=$(npm bin):$PATH
+
 ember build --environment=production -output-path=$WORKSPACE/{assets-outdirs}
 
 echo
 echo "Push rebuilt assets to git repository"
-
-git add $WORKSPACE/{assets-outdirs}
+cd $WORKSPACE
+git add {assets-outdirs}
 git commit -m "Build frontend assets for commit $GIT_COMMIT"
 git push origin HEAD:{scm_branch}
